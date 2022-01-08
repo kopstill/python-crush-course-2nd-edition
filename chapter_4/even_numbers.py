2a7a84a5b9b5f35adc5e8d05762465d582cb2e70
kopever <kop.still@gmail.com>
2022-01-08 18:08:42 +0800

    list range demo

---
+++ chapter_4/even_numbers.py
@@ -0,0 +1,2 @@
+even_numbers = list(range(2, 12, 2))
+print(even_numbers)
---
+++ chapter_4/first_numbers.py
@@ -0,0 +1,12 @@
+for value in range(1, 5):
+    print(value)
+
+print()
+
+for value in range(6):
+    print(value)
+
+print()
+
+numbers = list(range(6))
+print(numbers)
