0933dce25903ceb8d195e94f0f9a529918f2e59d
kopever <kop.still@gmail.com>
2022-01-08 16:01:49 +0800

    list practice

---
+++ chapter_3/guests.py
@@ -0,0 +1,30 @@
+guests = ['Turing', 'Einstein', 'Hawking']
+print(guests)
+
+canceled_guest = guests.pop(1)
+print(f'{canceled_guest} canceled')
+print(guests)
+
+new_guest = 'Newton'
+guests.append(new_guest)
+print(guests)
+
+more_guest_0 = 'Edison'
+guests.insert(0, more_guest_0)
+print(guests)
+more_guest_1 = 'Curie'
+guests.insert(2, more_guest_1)
+print(guests)
+more_guest_2 = 'Tesla'
+guests.append(more_guest_2)
+print(guests)
+
+guests.pop(0)
+guests.pop(0)
+guests.pop(0)
+guests.pop(0)
+print(guests)
+
+del guests[0]
+del guests[0]
+print(guests)
