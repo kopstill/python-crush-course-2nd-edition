1626b22b5c36f352fc19c14dc434f5e40077b840
kopever <kop.still@gmail.com>
2022-01-08 17:55:25 +0800

    for loop demo

---
+++ chapter_4/animals.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'rabbit']
+for animal in animals:
+    print(animal)
+    print(f'A {animal} would make a great pet.\n')
+
+print('Any of these animals would make a great pet!')
---
+++ chapter_4/pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['Pizza Hut', "Domino's Pizza", "Papa John's Pizza"]
+for pizza in pizzas:
+    print(pizza)
+    print(f'I like {pizza} pizza.\n')
+
+print("I really love pizza!")
