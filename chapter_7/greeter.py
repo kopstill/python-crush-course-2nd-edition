f5c28b5127d0b1951fd0ccb8f19205e94a6bf1d6
kopever <kop.still@gmail.com>
2022-01-20 11:54:40 +0800

    input demo

---
+++ chapter_7/greeter.py
@@ -0,0 +1,9 @@
+name = input("Please enter your name: ")
+print(f"Hello, {name.title()}!")
+
+print("#############")
+
+prompt = "If you tell us who you are, we can personlize the messages you see."
+prompt += "\nWhat is your first name? "
+name = input(prompt)
+print(f"Hello, {name.title()}!")
