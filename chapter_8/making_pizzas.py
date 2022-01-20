b3b4a07307de83680e4a3c6e317cce18196bdc55
kopever <kop.still@gmail.com>
2022-01-20 16:33:55 +0800

    import demo

---
+++ chapter_8/making_pizzas.py
@@ -0,0 +1,20 @@
+# import pizza
+# from pizza import make_pizza
+# from pizza import make_pizza as mp
+# import pizza as p
+from pizza import *
+
+# pizza.make_pizza(10, 'pepperoni')
+# pizza.make_pizza('16', 'mushrooms', 'green peppers', 'extra cheese')
+
+# make_pizza(10, 'pepperoni')
+# make_pizza('16', 'mushrooms', 'green peppers', 'extra cheese')
+
+# mp(10, 'pepperoni')
+# mp('16', 'mushrooms', 'green peppers', 'extra cheese')
+
+# p.make_pizza(10, 'pepperoni')
+# p.make_pizza('16', 'mushrooms', 'green peppers', 'extra cheese')
+
+make_pizza(10, 'pepperoni')
+make_pizza('16', 'mushrooms', 'green peppers', 'extra cheese')
