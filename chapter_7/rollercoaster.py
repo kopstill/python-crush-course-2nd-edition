b5e1a3c23c4583ed29b7650c1c4970279f5f9584
kopever <kop.still@gmail.com>
2022-01-20 11:55:04 +0800

    string to int demo

---
+++ chapter_7/rollercoaster.py
@@ -0,0 +1,7 @@
+height = input("How tall are you, in inches?\n")
+height = int(height)
+
+if height >= 48:
+    print("You're tall enough to ride!")
+else:
+    print("You'll be able to ride when you're a little older.")
