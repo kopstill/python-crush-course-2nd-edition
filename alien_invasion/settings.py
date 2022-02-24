043f4453cb85a39dd5f903e293ae3b4dba8a1b70
kopever <kop.still@gmail.com>
2022-02-24 18:15:39 +0800

    alien invasion
    basic game

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -6,9 +6,14 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         self.ship_speed = 10
+        self.ship_limit = 3
 
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_speed = 10.0
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 10
+
+        self.alien_speed = 10.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
