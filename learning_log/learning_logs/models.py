ac39a3cbc846787883a44d34eaed75074856695e
kopever <kop.still@gmail.com>
2022-03-11 19:13:48 +0800

    one-to-many demo

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -8,3 +8,18 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        return self.text
+    
