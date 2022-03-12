0e21d6c4cc1a2eb969410244d997cc00666a4d77
kopever <kop.still@gmail.com>
2022-03-12 23:57:46 +0800

    custom view

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 
 # Create your views here.
 def index(request):
     return render(request, 'learning_logs/index.html')
+
+
+def topics_page(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic_page(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
