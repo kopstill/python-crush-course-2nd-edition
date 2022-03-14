72632578e56ba243690115a77fc46f6c236095cd
kopever <kop.still@gmail.com>
2022-03-14 15:47:41 +0800

    add new topic entry page

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -6,6 +6,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics_page, name='topics'),
-    path('topics/<int:topic_id>/', views.topic_page, name='topic'),
-    path('new_topic/', views.new_topic, name='new_topic')
+    path('topic/<int:topic_id>/', views.topic_page, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    path('new_entry/<int:topic_id>/', views.new_entry_page, name='new_entry')
 ]
